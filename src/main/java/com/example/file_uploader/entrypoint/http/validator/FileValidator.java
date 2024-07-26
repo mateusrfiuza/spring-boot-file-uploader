e509169d54d0a7e5d184591ccfbf819da4fc5f94
Mateus Fiuza <mateus130@hotmail.com>
2024-07-26 12:05:51 -0300

    Added test for validation.

--- src/main/java/com/example/file_uploader/entrypoint/http/validator/FileValidator.java
+++ src/main/java/com/example/file_uploader/entrypoint/http/validator/FileValidator.java
@@ -11,11 +11,17 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileValidator implements ConstraintValidator<ValidFile, MultipartFile> {
 
     private static final Set<String> ALLOWED_FILE_EXTENSIONS = Set.of("application/pdf", "image/png", "image/jpg", "image/jpeg");
+    private static final String ERROR_FILE_ABSENT = "The file is required and cannot be empty.";
 
     @Override
     public boolean isValid(final MultipartFile multipartFile, final ConstraintValidatorContext context) {
 
         boolean result = true;
+
+        if (multipartFile == null || multipartFile.isEmpty()) {
+            setErrorMessage(context, ERROR_FILE_ABSENT);
+            return false;
+        }
 
         final var contentType = multipartFile.getContentType();
         if (Strings.isNotEmpty(contentType) && (!isSupportedContentType(contentType))) {
@@ -27,6 +33,12 @@ public class FileValidator implements ConstraintValidator<ValidFile, MultipartFi
 
     private boolean isSupportedContentType(final String contentType) {
         return ALLOWED_FILE_EXTENSIONS.contains(contentType);
+    }
+
+    private void setErrorMessage(final ConstraintValidatorContext context, final String errorMessage) {
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(errorMessage)
+            .addConstraintViolation();
     }
 
 }
--- src/test/java/com/example/file_uploader/entrypoint/http/validator/FileValidatorTest.java
+++ src/test/java/com/example/file_uploader/entrypoint/http/validator/FileValidatorTest.java
@@ -1,7 +1,57 @@
 package com.example.file_uploader.entrypoint.http.validator;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.ConstraintValidatorContext;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class FileValidatorTest {
+
+  @Mock
+  private ConstraintValidatorContext context;
+
+  @Mock
+  private ConstraintValidatorContext.ConstraintViolationBuilder constraintViolationBuilder;
+
+  @InjectMocks
+  private FileValidator fileValidator;
+
+
+  @Test
+  public void testShouldReturnTrueWhenIsValidFile() {
+    MultipartFile file = new MockMultipartFile("file", "valid_file.pdf", "application/pdf", "content".getBytes());
+    assertTrue(fileValidator.isValid(file, context));
+  }
+
+  @Test
+  public void testShouldReturnFalseWhenReceiveEmptyFile() {
+    when(context.buildConstraintViolationWithTemplate(anyString())).thenReturn(constraintViolationBuilder);
+    MultipartFile file = new MockMultipartFile("file", "valid_file.pdf", "application/pdf", new byte[0]);
+    assertFalse(fileValidator.isValid(file, context));
+  }
+
+  @Test
+  public void testShouldReturnFalseWhenReceiveNullFile() {
+    when(context.buildConstraintViolationWithTemplate(anyString())).thenReturn(constraintViolationBuilder);
+    assertFalse(fileValidator.isValid(null, context));
+  }
+
+  @Test
+  public void testShouldReturnFalseWhenReceiveInvalidFileFormat() {
+    MultipartFile file = new MockMultipartFile("file", "test.txt", "text/plain", "content".getBytes());
+    assertFalse(fileValidator.isValid(file, context));
+  }
+
 
 }
